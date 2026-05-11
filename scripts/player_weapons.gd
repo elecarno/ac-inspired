de321444366652cb3d824e4a5b4e721bb5ae1ba8
ElecarnoYT <benrobmc.mc@gmail.com>
2026-05-12 00:24:07 +0100

    projectile firing

--- scripts/player_weapons.gd
+++ scripts/player_weapons.gd
@@ -8,10 +8,10 @@ enum WEAPON_POSITION {
 
 # REFERENCES -------------------------------------------------------------------
 @onready var barrel_right_hand: Node3D = $"../mesh/weapon_barrels/barrel_right-hand"
-@onready var barrel_left_hand: Node3D = $"../mesh/weapon_barrels/barrel_left-hand"
+@onready var barrel_left_hand:  Node3D = $"../mesh/weapon_barrels/barrel_left-hand"
 @onready var barrel_right_back: Node3D = $"../mesh/weapon_barrels/barrel_right-back"
-@onready var barrel_left_back: Node3D = $"../mesh/weapon_barrels/barrel_left-back"
-
+@onready var barrel_left_back:  Node3D = $"../mesh/weapon_barrels/barrel_left-back"
+@onready var cam: Camera3D = $"../cam_pivot/cam_arm/cam"
 
 # PRELOADS ---------------------------------------------------------------------
 @onready var prefab_bullet: PackedScene = preload("res://bullet.tscn")
@@ -30,9 +30,16 @@ func _input(event: InputEvent) -> void:
 		
 
 func fire_weapon(weapon_pos: WEAPON_POSITION):
-	var bullet_instance: RigidBody3D = prefab_bullet.instantiate()
-	bullet_instance.global_position = barrel_right_hand.global_position
-	#if weapon_pos == WEAPON_POSITION.RIGHT_HAND:
-		#bullet_instance.global_position = barrel_right_hand.global_position
-
+	var barrel: Node3D = barrel_right_hand
+	if weapon_pos == WEAPON_POSITION.LEFT_HAND:
+		barrel = barrel_left_hand
+	if weapon_pos == WEAPON_POSITION.RIGHT_BACK:
+		barrel = barrel_right_back
+	if weapon_pos == WEAPON_POSITION.LEFT_BACK:
+		barrel = barrel_left_back
+	
+	var bullet_instance: Area3D = prefab_bullet.instantiate()
+	bullet_instance.position = barrel.global_position
+	bullet_instance.rotation = cam.global_rotation
+		
 	get_parent().get_parent().add_child(bullet_instance)
--- scripts/proj_bullet.gd
+++ scripts/proj_bullet.gd
@@ -1,4 +1,9 @@
-extends RigidBody3D
+extends Area3D
 
+# EXPORTS ----------------------------------------------------------------------
+@export var speed: float = 50.0
+
+
+# CODE -------------------------------------------------------------------------
 func _process(delta: float) -> void:
-	pass
+	position += transform.basis * Vector3(0, 0, -speed) * delta
